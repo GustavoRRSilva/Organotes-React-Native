f58e2c3a2e9e6c1b22470952c471ae6c5918b867
Gustavo <gstvrrds@gmail.com>
2025-04-22 23:14:57 -0300

    feat:adding pie graph

--- components/ActivitiesInfosHead/index.tsx
+++ components/ActivitiesInfosHead/index.tsx
@@ -1,6 +1,6 @@
 import { ActiviesPage, ActivitiesInfosHeadProps } from "@/types/types";
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { PieChart } from "react-native-gifted-charts";
 import PieChartComponent from "../PieChart";
 import Colors from "@/constants/Colors";
@@ -12,6 +12,31 @@ export default function ActivitiesInfosHead({
     name: atividade.name,
     value: atividade.pendingSubject.length,
     color: Colors.light.pieChartColors[index],
+    pressed: false,
+    onPress: () => {
+      console.log("Fatia pressionada" + atividade.name);
+    },
   }));
-  return <PieChartComponent data={pieChartInfos} />;
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+      <PieChartComponent data={pieChartInfos} />
+      <View style={{ justifyContent: "space-around", width: "40%" }}>
+        {pieChartInfos.map((subject) => (
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 2 }}>
+            <View
+              style={{
+                backgroundColor: subject.color,
+                width: 20,
+                height: 20,
+                borderRadius: "100%",
+              }}
+            >
+              {" "}
+            </View>
+            <Text style={{ maxWidth: 130 }}>{subject.name}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
 }
--- components/PieChart/index.tsx
+++ components/PieChart/index.tsx
@@ -1,20 +1,41 @@
 import { PieChartProps } from "@/types/types";
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { PieChart } from "react-native-gifted-charts";
 
 export default function PieChartComponent({ data }: PieChartProps) {
   return (
-    <View style={{ maxWidth: 50 }}>
+    <View
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        position: "relative",
+      }}
+    >
       <PieChart
         data={data}
-        radius={150}
+        radius={100}
         textSize={20}
         focusOnPress
         textBackgroundRadius={26}
-        innerRadius={80}
+        innerRadius={60}
         donut
       />
+      <Text
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: [
+            { translateX: -40 }, // metade da largura estimada do texto
+            { translateY: -10 }, // metade da altura estimada do texto
+          ],
+          fontSize: 16,
+          fontWeight: "bold",
+        }}
+      >
+        {data.length} Matérias
+      </Text>
     </View>
   );
 }

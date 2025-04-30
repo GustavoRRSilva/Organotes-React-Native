bb8cc2c7e86bb84ab79680c8f5cd2a2e85cc3edb
Gustavo <gstvrrds@gmail.com>
2025-04-30 20:10:48 -0300

    feat:solvin nativewind bug

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 export default function Index() {
   const router = useRouter();
-  setTimeout(() => router.replace("/activitiesPage/page"), 2000);
+  setTimeout(() => router.replace("/auth/Login/page"), 2000);
   return (
     <SafeAreaView style={{ backgroundColor: Colors.light.background, flex: 1 }}>
       <View style={styles.container}>
